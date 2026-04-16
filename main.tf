55333f5c69094554e8610807b9dd38b725331fd4
krishna <chaitupuppala20@gmail.com>
2026-04-16 15:37:55 +0100

    Added ci-cd

---
+++ main.tf
@@ -0,0 +1,40 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+# MongoDB PVC
+resource "kubernetes_manifest" "mongo_pvc" {
+  manifest = yamldecode(file("${path.module}/k8s/mongo-pvc.yaml"))
+}
+
+# MongoDB Deployment
+resource "kubernetes_manifest" "mongodb" {
+  manifest = yamldecode(file("${path.module}/k8s/mongodb.yaml"))
+}
+
+# MongoDB Service
+resource "kubernetes_manifest" "mongodb_service" {
+  manifest = yamldecode(file("${path.module}/k8s/mongodb-service.yaml"))
+}
+
+# Backend Deployment
+resource "kubernetes_manifest" "backend" {
+  manifest = yamldecode(file("${path.module}/k8s/backend.yaml"))
+  depends_on = [kubernetes_manifest.mongodb]
+}
+
+# Backend Service
+resource "kubernetes_manifest" "backend_service" {
+  manifest = yamldecode(file("${path.module}/k8s/backend-service.yaml"))
+}
+
+# Frontend Deployment
+resource "kubernetes_manifest" "frontend" {
+  manifest = yamldecode(file("${path.module}/k8s/frontend.yaml"))
+  depends_on = [kubernetes_manifest.backend]
+}
+
+# Frontend Service
+resource "kubernetes_manifest" "frontend_service" {
+  manifest = yamldecode(file("${path.module}/k8s/frontend-service.yaml"))
+}
